1ea3d9bc2a82b01ca3588f44762e551a11766e5c
Nabil <nbfahrinadi@gmail.com>
2025-07-20 01:15:02 +0700

    update content, add links, update html, css, js

--- script.js
+++ script.js
@@ -6,3 +6,35 @@ window.addEventListener('scroll', function () {
     navbar.classList.remove('scrolled');
   }
 });
+
+document.querySelectorAll('*').forEach(el => el.setAttribute('spellcheck', 'false'));
+
+const modal = document.getElementById("myModal");
+const closeBtn = document.getElementById("closeModalBtn");
+const openButtons = [
+  document.getElementById("openNavbarModal"),
+  document.getElementById("openHeroModal"),
+  document.getElementById("openNavbarModalContact"),
+  document.getElementById("openFooterModalContact"),
+];
+
+// Buka modal (dengan class .show)
+openButtons.forEach(button => {
+  button.addEventListener("click", function (event) {
+    event.preventDefault();
+    modal.classList.add("show");
+  });
+});
+
+// Tutup modal (hapus class .show)
+closeBtn.addEventListener("click", function () {
+  modal.classList.remove("show");
+});
+
+// Klik di luar modal
+window.addEventListener("click", function (event) {
+  if (event.target === modal) {
+    modal.classList.remove("show");
+  }
+});
+
